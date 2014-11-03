9d1595e15cb851c167945501d2dbde8216f9b463
Michael Mendenhall <michael.mendenhall@nist.gov>
2014-11-03 16:44:22 -0500

    Adding decay sources event generator code

---
+++ include/Output/strutils.hh
@@ -0,0 +1,68 @@
+#ifndef STRUTILS_HH
+#define STRUTILS_HH
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+
+using std::string;
+using std::vector;
+
+/// utility function for converting to string
+template<typename T>
+string to_str(T x) {
+    std::stringstream ss;
+    ss << x;
+    return ss.str();
+}
+
+/// convert a double to a string
+string dtos(double d);
+/// convert an int to a string
+string itos(int i);
+/// small integer to roman numerals string
+string itosRN(int i);
+
+/// convert a vector of doubles to a string list
+string vtos(const double* st, const double* en, string sep = ",");
+/// convert a vector of doubles to a string list
+string vtos(const vector<double>& ds,string sep = ",");
+/// convert a vector of floats to a string list
+string vtos(const float* st, const float* en, string sep = ",");
+/// convert a vector of doubles to a string list
+string vtos(const vector<float>& ds,string sep = ",");
+/// convert a vector of ints to a string list
+string vtos(const int* st, const int* en, string sep = ",");
+/// convert a vector of ints to a string list
+string vtos(const vector<int>& ds,string sep = ",") ;
+
+/// convert a char to a string
+string ctos(char c);
+/// convert a string to lowercase
+string lower(string s);
+/// convert a string to uppercase
+string upper(string s);
+/// replace all of one character in a string with another
+string replace(string str, char o, char n);
+/// check whether string a begins with string b
+bool startsWith(const string& a, const string& b);
+/// split a string into substrings on given split characters
+vector<string> split(const string& str, const string splitchars = " \t\r\n");
+/// join a list of strings into a single string
+string join(const vector<string>& ss, const string& sep = " ");
+/// strip junk chars off start and end of string
+string strip(const string& str, const string stripchars = " \t\r\n");
+/// split a string into a vector of doubles
+vector<double> sToDoubles(const string& str, const string splitchars = ", \t\r\n");
+/// split a string into a vector of floats
+vector<float> sToFloats(const string& str, const string splitchars = ", \t\r\n");
+/// split a string into a vector of ints
+vector<int> sToInts(const string& str, const string splitchars = ", \t\r\n");
+/// read in an array from a file
+vector< vector<float> > readArray(std::ifstream& fin, unsigned int minitems = 1, const string splitchars = ", \t\r\n");
+
+#endif
---
+++ src/Output/Stringmap.cc
@@ -0,0 +1,208 @@
+#include "Stringmap.hh"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <utility>
+#include "strutils.hh"
+#include "PathUtils.hh"
+#include "SMExcept.hh"
+
+Stringmap::Stringmap(const string& str) {
+    vector<string> pairs = split(str,"\t");
+    for(vector<string>::const_iterator it = pairs.begin(); it!=pairs.end(); it++) {
+        vector<string> keyval = split(*it,"=");
+        if(keyval.size() != 2)
+            continue;
+        dat.insert(std::make_pair(strip(keyval[0]),strip(keyval[1])));
+    }
+}
+
+Stringmap::Stringmap(const Stringmap& m) {
+    for(std::multimap< string, string >::const_iterator it = m.dat.begin(); it!=m.dat.end(); it++)
+        dat.insert(std::make_pair(it->first,it->second));
+}
+
+void Stringmap::insert(const string& s, const string& v) {
+    dat.insert(std::make_pair(s,v));
+}
+
+void Stringmap::insert(const string& s, double d) {
+    insert(s,dtos(d));
+}
+
+void Stringmap::erase(const string& s) { dat.erase(s); }
+
+vector<string> Stringmap::retrieve(const string& s) const {
+    vector<string> v;
+    for(std::multimap<string,string>::const_iterator it = dat.lower_bound(s); it != dat.upper_bound(s); it++)
+        v.push_back(it->second);
+    return v;
+}
+
+string Stringmap::getDefault(const string& s, const string& d) const {
+    std::multimap<string,string>::const_iterator it = dat.find(s);
+    if(it == dat.end())
+        return d;
+    return it->second;
+}
+
+string Stringmap::toString() const {
+    string s;
+    for(std::multimap<string,string>::const_iterator it = dat.begin(); it != dat.end(); it++)
+        s += "\t" + it->first + " = " + it->second;
+    return s;
+}
+
+void Stringmap::display(string linepfx) const {
+    for(std::multimap<string,string>::const_iterator it = dat.begin(); it != dat.end(); it++)
+        std::cout << linepfx <<	it->first << ": " << it->second << "\n";
+}
+
+
+double Stringmap::getDefault(const string& k, double d) const {
+    string s = getDefault(k,"");
+    if(!s.size())
+        return d;
+    std::stringstream ss(s);
+    ss >> d;
+    return d;
+}
+
+vector<double> Stringmap::retrieveDouble(const string& k) const {
+    vector<string> vs = retrieve(k);
+    vector<double> v;
+    double d;
+    for(vector<string>::const_iterator it = vs.begin(); it != vs.end(); it++) {
+        std::stringstream s(*it);
+        s >> d;
+        v.push_back(d);
+    }
+    return v;
+}
+
+void Stringmap::mergeInto(Stringmap& S) const {
+    for(std::multimap<string,string>::const_iterator it = dat.begin(); it != dat.end(); it++)
+        S.insert(it->first,it->second);	
+}
+
+//-------------------------------------------------
+
+QFile::QFile(const std::string& fname, bool readit) {
+    name = fname;
+    if(!readit || name=="")
+        return;
+    if(!fileExists(fname)) {
+        SMExcept e("fileUnreadable");
+        e.insert("filename",fname);
+        throw(e);
+    }
+    std::ifstream fin(fname.c_str());
+    string s;
+    while (fin.good()) {
+        std::getline(fin,s);
+        s = strip(s);
+        size_t n = s.find(':');
+        if(n==std::string::npos || s[0]=='#') continue;
+        string key = s.substr(0,n);
+        string vals = s.substr(n+1);
+        vals=strip(vals);
+        while(vals.size() && vals[vals.size()-1]=='\\') {
+            vals.erase(vals.size()-1);
+            std::getline(fin,s);
+            s = strip(s);
+            vals += '\t';
+        vals += s;
+        }
+        insert(key,Stringmap(vals));
+    }
+    fin.close();
+}
+
+void QFile::insert(const std::string& s, const Stringmap& v) {
+    dat.insert(std::make_pair(s,v));
+}
+
+void QFile::erase(const std::string& s) { dat.erase(s); }
+
+vector<Stringmap> QFile::retrieve(const std::string& s) const {
+    vector<Stringmap> v;
+    for(std::multimap<std::string,Stringmap>::const_iterator it = dat.lower_bound(s); it != dat.upper_bound(s); it++)
+        v.push_back(it->second);
+    return v;
+}
+
+void QFile::transfer(const QFile& Q, const std::string& k) {
+    vector<Stringmap> v = Q.retrieve(k);
+    for(vector<Stringmap>::iterator it = v.begin(); it != v.end(); it++)
+        insert(k,*it);
+}
+
+void QFile::display() const {
+    for(std::multimap<std::string, Stringmap>::const_iterator it = dat.begin(); it != dat.end(); it++) {
+        std::cout << "--- " << it->first << " ---:\n";
+        it->second.display();
+    }
+}
+
+void QFile::commit(string outname) const {
+    if(outname=="")
+        outname = name;
+    makePath(outname,true);
+    std::ofstream fout(outname.c_str());
+    if(!fout.good()) {
+        SMExcept e("fileUnwriteable");
+        e.insert("filename",outname);
+        throw(e);
+    }
+    printf("Writing File '%s'.\n",outname.c_str());
+    for(std::multimap<std::string, Stringmap>::const_iterator it = dat.begin(); it != dat.end(); it++)
+        fout << it->first << ":\t" << it->second.toString() << "\n";
+    fout.close();
+}
+
+vector<string> QFile::retrieve(const std::string& k1, const std::string& k2) const {
+    vector<string> v1;
+    for(std::multimap<std::string,Stringmap>::const_iterator it = dat.lower_bound(k1); it != dat.upper_bound(k1); it++) {
+        vector<string> v2 = it->second.retrieve(k2);
+        for(vector<string>::const_iterator it2 = v2.begin(); it2 != v2.end(); it2++)
+            v1.push_back(*it2);
+    }
+    return v1;
+}
+
+vector<double> QFile::retrieveDouble(const std::string& k1, const std::string& k2) const {
+    vector<double> v1;
+    for(std::multimap<std::string,Stringmap>::const_iterator it = dat.lower_bound(k1); it != dat.upper_bound(k1); it++) {
+        vector<double> v2 = it->second.retrieveDouble(k2);
+        for(vector<double>::const_iterator it2 = v2.begin(); it2 != v2.end(); it2++)
+            v1.push_back(*it2);
+    }
+    return v1;
+}
+
+string QFile::getDefault(const std::string& k1, const std::string& k2, const std::string& d) const {
+    for(std::multimap<std::string,Stringmap>::const_iterator it = dat.lower_bound(k1); it != dat.upper_bound(k1); it++) {
+        vector<string> v2 = it->second.retrieve(k2);
+        if(v2.size())
+            return v2[0];
+    }
+    return d;
+}
+
+double QFile::getDefault(const std::string& k1, const std::string& k2, double d) const {
+    for(std::multimap<std::string,Stringmap>::const_iterator it = dat.lower_bound(k1); it != dat.upper_bound(k1); it++) {
+        vector<double> v2 = it->second.retrieveDouble(k2);
+        if(v2.size())
+            return v2[0];
+    }
+    return d;
+}
+
+Stringmap QFile::getFirst(const std::string& s, const Stringmap& dflt) const {
+    std::multimap<std::string,Stringmap>::const_iterator it = dat.find(s);
+    if(it == dat.end())
+        return dflt;
+    return it->second;
+}
+
