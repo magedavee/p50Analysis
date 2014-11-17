1899431ed0c9df310f8f4723f9504b0b010523e9
Michael Mendenhall <michael.mendenhall@nist.gov>
2014-11-17 09:18:17 -0500

    Switchable low-energy EM physics list

--- include/Physics/PhysicsList.hh
+++ include/Physics/PhysicsList.hh
@@ -1,73 +1,49 @@
 #ifndef PHYSICSLIST_HH
 #define PHYSICSLIST_HH
 
-//#define EMPHYSICS
-
 #include "XMLProvider.hh"
-
-#ifndef EMPHYSICS
+#include "StepMax.hh"
 
 #include <G4UImessenger.hh>
 #include <QGSP_BERT_HP.hh>
+#include <G4VModularPhysicsList.hh>
+#include <G4EmStandardPhysics.hh>
+#include <G4EmLivermorePhysics.hh>
+#include <G4EmPenelopePhysics.hh>
 #include <G4OpticalPhysics.hh>
+
 #include <G4UIdirectory.hh>
 #include <G4UIcmdWithoutParameter.hh>
+#include <G4UIcmdWithADoubleAndUnit.hh>
 
 class PhysicsListMessenger;
 
 /// Physics list, derived from QGSP_BERT_HP
-class PhysicsList: public QGSP_BERT_HP, public XMLProvider {
-    friend class PhysicsListMessenger;
+class PhysicsList: public QGSP_BERT_HP, public G4UImessenger, public XMLProvider {
 public:
     /// Constructor
     PhysicsList();
     /// Destructor
     ~PhysicsList();
+    
+    /// Response to UI commands
+    void SetNewValue(G4UIcommand* command, G4String newValue);
         
+    virtual void ConstructProcess();
+    virtual void SetCuts();
+    
 protected:
-    PhysicsListMessenger* myMessenger;  ///< UI for this PhysicsList
-    G4OpticalPhysics* myOptPhys;        ///< optical physics processes
+    G4OpticalPhysics* myOptPhys = NULL;         ///< optical physics processes
+    G4VPhysicsConstructor* myEMPhys = NULL;     ///< low-energy electromagnetic physics
+    StepMax* myStepMax = NULL;                  ///< optional step-size limit
+    
+    G4UIdirectory physDir;                      ///< UI directory for physics commands
+    G4UIcmdWithoutParameter opticalCmd;         ///< command for enabling optical physics
+    G4UIcmdWithoutParameter emCmd;              ///< command for enabling precision electromagnetic physics
+    G4UIcmdWithADoubleAndUnit stepMaxCmd;       ///< command for step-size limiter
     
     /// XML output contents
     virtual void fillNode(TXMLEngine& E);
 };
-
-/// UI messenger class for physics list
-class PhysicsListMessenger: public G4UImessenger {
-public:
-    /// Constructor
-    PhysicsListMessenger(PhysicsList* p);
-    /// Destructor
-    ~PhysicsListMessenger();
-    
-    /// Response to UI commands
-    void SetNewValue(G4UIcommand* command, G4String newValue);
-    
-protected:
-    PhysicsList* phys;                  ///< physics list being controlled
-    G4UIdirectory* physDir;             ///< UI directory for physics commands
-    G4UIcmdWithoutParameter* opticalCmd;///< command for enabling optical physics
-};
-
-#else
-
-#include <G4VModularPhysicsList.hh>
-#include <G4EmStandardPhysics.hh>
-#include <G4EmLivermorePhysics.hh>
-#include <G4EmPenelopePhysics.hh>
-
-/// Low-energy electromagnetic physics list
-class PhysicsList: public G4VModularPhysicsList, public XMLProvider {
-public:
-    /// Constructor
-    PhysicsList(): XMLProvider("EMPhysics") { SetDefaultCutValue(0.001); emPhysicsList = new G4EmStandardPhysics(); }
-    virtual void ConstructParticle() { emPhysicsList->ConstructParticle(); }
-    virtual void ConstructProcess() { AddTransportation(); emPhysicsList->ConstructProcess(); AddStepMax(); }
-    virtual void SetCuts();
-    virtual void AddStepMax();
-    G4VPhysicsConstructor* emPhysicsList;
-};
-
-#endif
 
 #endif
--- src/Physics/PhysicsList.cc
+++ src/Physics/PhysicsList.cc
@@ -1,70 +1,76 @@
 #include "PhysicsList.hh"
 
-#ifndef EMPHYSICS
+#include <G4UnitsTable.hh>
+#include <G4SystemOfUnits.hh>
+#include <G4ProductionCutsTable.hh>
 
 PhysicsList::PhysicsList(): XMLProvider("Physics"),
-myMessenger(new PhysicsListMessenger(this)), myOptPhys(NULL) {
+physDir("/phys/"),
+opticalCmd("/phys/enableOptical",this),
+emCmd("/phys/enableEM",this),
+stepMaxCmd("/phys/stepMax",this) {
+    
+    opticalCmd.SetGuidance("Enables optical photon physics.");
+    opticalCmd.AvailableForStates(G4State_PreInit,G4State_Init);
+    
+    emCmd.SetGuidance("Enables precision low-energy electromagnetic physics.");
+    emCmd.AvailableForStates(G4State_PreInit,G4State_Init);
+    
+    stepMaxCmd.SetGuidance("Set limit on maximum step length.");
+    stepMaxCmd.AvailableForStates(G4State_PreInit);
 }
 
 PhysicsList::~PhysicsList() {
-    delete myMessenger;
     // Do not delete myOptPhys --- automatically handled
 }
 
-void PhysicsList::fillNode(TXMLEngine& E) {
-    addAttr(E, "optical", myOptPhys?"on":"off");
-}
-
-PhysicsListMessenger::PhysicsListMessenger(PhysicsList* p): 
-phys(p), physDir(new G4UIdirectory("/phys/")) {    
-    opticalCmd = new G4UIcmdWithoutParameter("/phys/enableOptical",this);
-    opticalCmd->SetGuidance("Enables optical photon physics.");
-    opticalCmd->AvailableForStates(G4State_PreInit,G4State_Init);
-}
-
-PhysicsListMessenger::~PhysicsListMessenger() {
-    delete physDir;
-    delete opticalCmd;
-}
-
-void PhysicsListMessenger::SetNewValue(G4UIcommand* command, G4String) {
-    if(command == opticalCmd) {
-        if(!phys->myOptPhys) {
-            phys->myOptPhys = new G4OpticalPhysics(0);
-            phys->RegisterPhysics(phys->myOptPhys);
+void PhysicsList::ConstructProcess() {
+    if(myEMPhys) {
+        AddTransportation();
+        myEMPhys->ConstructProcess();
+    } else QGSP_BERT_HP::ConstructProcess();
+    
+    if(myStepMax) {
+        printf("Setting step-size limits...\n");
+        theParticleIterator->reset();
+        while ((*theParticleIterator)()){
+            G4ParticleDefinition* particle = theParticleIterator->value();
+            if (myStepMax->IsApplicable(*particle)) particle->GetProcessManager()->AddDiscreteProcess(myStepMax);
         }
     }
 }
-
-#else
-
-#include "StepMax.hh"
-#include <G4SystemOfUnits.hh>
-#include <G4ProductionCutsTable.hh>
-#include <G4ProcessManager.hh>
 
 void PhysicsList::SetCuts() {
-    G4ProductionCutsTable::GetProductionCutsTable()->SetEnergyRange(250*eV, 1*GeV);
-    double rangecut = 1*um;
-    SetCutValue(rangecut, "gamma");
-    SetCutValue(rangecut, "e-");
-    SetCutValue(rangecut, "e+");
-}
-
-void PhysicsList::AddStepMax() {
-    StepMax* stepMaxProcess = new StepMax();
-    
-    printf("Setting step-size limits...\n");
-    
-    theParticleIterator->reset();
-    while ((*theParticleIterator)()){
-        G4ParticleDefinition* particle = theParticleIterator->value();
-        G4ProcessManager* pmanager = particle->GetProcessManager();
-        if (stepMaxProcess->IsApplicable(*particle)) {
-            printf("\tSetting step limit for %s\n", particle->GetParticleName().c_str());
-            pmanager->AddDiscreteProcess(stepMaxProcess);
-        }
+    QGSP_BERT_HP::SetCuts();
+    if(myEMPhys) {
+        G4ProductionCutsTable::GetProductionCutsTable()->SetEnergyRange(250*eV, 1*GeV);
+        double rangecut = 1*um;
+        SetCutValue(rangecut, "gamma");
+        SetCutValue(rangecut, "e-");
+        SetCutValue(rangecut, "e+");
     }
 }
 
-#endif
+
+void PhysicsList::fillNode(TXMLEngine& E) {
+    addAttr(E, "optical", myOptPhys?"on":"off");
+    addAttr(E, "EM", myEMPhys?"on":"off");
+    if(myStepMax) addAttr(E, "stepmax", G4BestUnit(myStepMax->GetMaxStep(),"Length"));
+}
+
+void PhysicsList::SetNewValue(G4UIcommand* command, G4String newValue) {
+    if(command == &opticalCmd) {
+        if(!myOptPhys) {
+            myOptPhys = new G4OpticalPhysics(0);
+            RegisterPhysics(myOptPhys);
+        }
+    } else if(command == &emCmd) {
+        if(!myEMPhys) {
+            myEMPhys = new G4EmLivermorePhysics();
+            //RegisterPhysics(myEMPhys);
+        }
+    } else if(command == &stepMaxCmd) {
+        if(!myStepMax) myStepMax = new StepMax();
+        myStepMax->SetMaxStep(stepMaxCmd.GetNewDoubleValue(newValue));
+    }
+}
