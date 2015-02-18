3bc8d0678f690d17a44724ff283b408cba2e149c
Michael Mendenhall <michael.mendenhall@nist.gov>
2015-02-18 12:26:32 -0500

    Update LinkDef so reading ROOT output works again.

--- include/Output/EventLinkDef.h
+++ include/Output/EventLinkDef.h
@@ -4,10 +4,13 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ struct s_ParticleVertex+;
 #pragma link C++ class ParticleVertex+;
 #pragma link C++ class ParticleEvent+;
+#pragma link C++ struct s_IoniCluster+;
 #pragma link C++ class IoniCluster+;
 #pragma link C++ class IoniClusterEvent+;
+#pragma link C++ struct s_NCapt+;
 #pragma link C++ class NCapt+;
 #pragma link C++ class NCaptEvent+;
 #pragma link C++ class Event+;
