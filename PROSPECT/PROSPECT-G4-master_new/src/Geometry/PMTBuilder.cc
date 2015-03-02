f4fa1d0f942ea4a02d446e0aaf900fd0b816db60
Daniel Davee <magedavee>
2015-03-02 10:31:36 -0500

    This adds Optical physics and a new branch to the ROOT tree Optical, which registers and particle inside PMT

---
+++ PROSPECT/PROSPECT-G4-master_new/src/Geometry/PMTBuilder.cc
@@ -0,0 +1,50 @@
+#include "PMTBuilder.hh"
+#include "MaterialsHelper.hh"
+
+#include <G4SystemOfUnits.hh>
+#include <G4UnitsTable.hh>
+#include <G4Tubs.hh>
+#include <G4Box.hh>
+#include <G4PVPlacement.hh>
+#include <OptCounterSD.hh>
+#include <cmath>
+#include <G4SDManager.hh>
+PMTBuilder::PMTBuilder(): Builder("PMT"), block_width(0), block_length(20*cm),
+diameter(5*in), length(0),
+outer_vis(G4Color(0.2,0.2,0.4)), capsule_vis(G4Color(1.0,1.0,0.0)) { }
+
+void PMTBuilder::construct() {
+    length = 1.0*diameter;
+    double capsule_length = 0.66*length;        // length of vacuum capsule
+    double capsule_radius = diameter/2. - 2.*mm;// radius of vacuum capsule
+    double capsule_thick = 5.*mm;               // thickness of vacuum capsule
+    
+    dim = G4ThreeVector(block_width, block_width, block_length);
+    
+    G4Box* housing_box = new G4Box("pmt_housing", dim[0]/2, dim[1]/2, dim[2]/2);
+    main_log = new G4LogicalVolume(housing_box, MaterialsHelper::M().PMMA_white, "PMT_main_log");
+    main_log->SetVisAttributes(&outer_vis);
+    
+    G4Tubs* pmt_tube = new G4Tubs("pmt_tube", 0, diameter/2., length/2., 0, 2*M_PI);
+    G4LogicalVolume* tube_log = new G4LogicalVolume(pmt_tube, MaterialsHelper::M().SS444, "PMT_tube_log");
+    new G4PVPlacement(NULL, G4ThreeVector(0,0,(block_length-length)/2.), tube_log, "PMT_tube_phys", main_log, false, 0, true);
+    
+    G4Tubs* pmt_capsule_tube = new G4Tubs("pmt_capsule_tube", 0, capsule_radius, capsule_length/2., 0, 2*M_PI);
+    G4LogicalVolume* capsule_log = new G4LogicalVolume(pmt_capsule_tube, MaterialsHelper::M().Quartz, "PMT_capsule_log");
+    capsule_log->SetVisAttributes(&capsule_vis);
+    new G4PVPlacement(NULL, G4ThreeVector(0,0,(length-capsule_length)/2.), capsule_log, "PMT_capsule_phys", tube_log, false, 0, true);
+    
+    G4Tubs* pmt_vacuum_tube = new G4Tubs("pmt_vacuum_tube", 0, capsule_radius - capsule_thick, capsule_length/2.-capsule_thick, 0, 2*M_PI);
+    G4LogicalVolume* vacuum_log = new G4LogicalVolume(pmt_vacuum_tube, MaterialsHelper::M().Vacuum, "PMT_vacuum_log");
+    new G4PVPlacement(NULL, G4ThreeVector(0,0,0), vacuum_log, "PMT_vacuum_phys", capsule_log, false, 0, true);
+    
+    OptCounterSD* mySD = new OptCounterSD("OpticalSD");
+    G4SDManager::GetSDMpointer()->AddNewDetector(mySD);
+    main_log->SetSensitiveDetector(mySD);
+}
+
+void PMTBuilder::fillNode(TXMLEngine& E) {
+    addAttr(E, "len", G4BestUnit(length,"Length"));
+    addAttr(E, "d", G4BestUnit(diameter,"Length"));
+    addAttr(E, "dim", G4BestUnit(dim,"Length"));
+}
---
+++ PROSPECT/PROSPECT-G4-master_new/src/Output/RootIO.cc
@@ -0,0 +1,119 @@
+#include <sstream>
+#include <cassert>
+
+#include <TFile.h>
+#include <TTree.h>
+
+#include "RootIO.hh"
+
+#include <G4ios.hh>
+
+
+static RootIO* instance = NULL;
+
+RootIO::RootIO(): writecount(0), outfile(NULL), dataTree(NULL) {
+    dataTree = new TTree("PG4","PROSPECT Geant4 simulation");
+    dataTree->SetDirectory(NULL);
+    addPrimBranch();
+    addEvtBranch();
+    addOpticalBranch();
+    G4cout << "RootIO initialized. Remember to specify output filename." << G4endl;
+}
+
+void RootIO::Clear() {
+    for(auto it = subObjs.begin(); it != subObjs.end(); it++)
+        (*it)->Clear();
+}
+
+RootIO* RootIO::GetInstance() {
+    if (instance == NULL) {
+        G4cout << "Instantiating ROOT output instance" << G4endl;
+        instance = new RootIO();
+    }
+    return instance;
+}
+
+void RootIO::WriteFile() {
+    if(!outfile) {
+        G4cout << "No ROOT output file opened! Data not saved!" << G4endl;
+        return;
+    } else outfile->cd();
+    
+    std::cout << "Writing events to file '" << fname << "' and closing..." << std::endl;
+    if(dataTree) dataTree->Write();
+    outfile->Write();
+    outfile->Close();
+    delete outfile;
+    outfile = NULL;
+}
+
+void RootIO::FillTree() {
+    if(!dataTree) return;
+    dataTree->Fill();
+    writecount++;
+    if(writecount==500) {
+        dataTree->AutoSave("SaveSelf");
+        writecount=0;
+    }
+}
+
+
+
+void RootIO::addEvtBranch() {
+    if(pmcevent) return; // already set up
+    G4cout << "RootIO Setting up 'Evt' output branch...\n";
+    subObjs.push_back(pmcevent = &mcevent);
+    dataTree->Branch("Evt",&pmcevent);
+}
+
+void RootIO::addPrimBranch() {
+    if(pprimPtcls) return; // already set up
+    G4cout << "RootIO Setting up 'Prim' output branch...\n";
+    subObjs.push_back(pprimPtcls = &primPtcls);
+    dataTree->Branch("Prim",&pprimPtcls);
+}
+
+void RootIO::addOpticalBranch() {
+    if(poptPhotoCounter) return; // already set up
+    G4cout << "RootIO Setting up 'Optical' output branch...\n";
+    subObjs.push_back(poptPhotoCounter = &optPhotoCounter);
+    dataTree->Branch("Optical",&poptPhotoCounter);
+}
+
+void RootIO::addScIoniBranch() {
+    if(pscintIoni) return; // already set up
+    G4cout << "RootIO Setting up 'ScIoni' output branch...\n";
+    subObjs.push_back(pscintIoni = &scintIoni);
+    dataTree->Branch("ScIoni",&pscintIoni);
+}
+
+void RootIO::addNCaptBranch() {
+    if(pscintNCapt) return; // already set up
+    G4cout << "RootIO Setting up 'ScN' output branch...\n";
+    subObjs.push_back(pscintNCapt = &scintNCapt);
+    dataTree->Branch("ScN",&pscintNCapt);
+}
+
+void RootIO::addFluxBranch() {
+    if(pfluxCounter) return; // already set up
+    G4cout << "RootIO Setting up 'Flux' output branch...\n";
+    subObjs.push_back(pfluxCounter = &fluxCounter);
+    dataTree->Branch("Flux",&pfluxCounter);
+}
+
+void RootIO::addVetoIoniBranch() {
+    if(pvetoIoni) return; // already set up
+    G4cout << "RootIO Setting up 'VetoIoni' output branch...\n";
+    subObjs.push_back(pvetoIoni = &vetoIoni);
+    dataTree->Branch("VetoIoni",&pvetoIoni);
+}
+
+
+
+void RootIO::SetFileName(G4String filename) {
+    fname = filename;
+    G4cout << "RootIO: Setting output file to '" << fname << "'\n";
+    outfile = new TFile(filename,"RECREATE");
+    outfile->cd();
+    dataTree->SetDirectory(outfile);
+}
